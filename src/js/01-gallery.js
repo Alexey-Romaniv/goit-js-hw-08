741f8e2899860ae610f36b4913928345aea61531
Alexey-Romaniv <105770723+Alexey-Romaniv@users.noreply.github.com>
2022-08-31 08:00:37 +0300

    made  1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `
+      <a class="gallery__item" href=${original} >
+    <img
+      class="gallery__image"
+      src=${preview}
+      alt=${description}
+      />
+      </a>`
+  )
+  .join('');
+
+galleryList.innerHTML = markup;
+
+galleryList.addEventListener('click', e => {
+  return e.preventDefault();
+});
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsType: 'alt',
+  captionDelay: 250,
+  captionPosiotion: 'bottom',
+});
